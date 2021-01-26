35b1811c884e074800bc62cb63f575bae07d7cfb
YogeshRaoJi <yogeshrao11091997@gmail.com>
2021-01-26 09:54:15 +0530

    identity

---
+++ SSEInvoice/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SSEInvoice.Data;
+
+[assembly: HostingStartup(typeof(SSEInvoice.Areas.Identity.IdentityHostingStartup))]
+namespace SSEInvoice.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
